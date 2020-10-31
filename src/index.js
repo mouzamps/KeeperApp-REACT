135da249070c2017cf02a2d217adb5f06edcfea9
mouzam <mzmjuly9@gmail.com>
2020-10-31 20:29:53 +0530

    practised passing expressions

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const fname = "mouzam";
-const lname = "ps";
-const number = 10;
- ReactDOM.render(
-   <div>
-     <h1>Hi {fname} {lname}</h1>
-     <p>my lucky number is {number}</p>
-   </div>,
-   document.getElementById('root')
- );
+const username = "Mouzam";
+const year = new Date().getFullYear()
+
+ReactDOM.render(
+  <div>
+  <p>Created by {username}</p>
+  <p>Copyright {year}</p>
+  </div>,
+  document.getElementById('root')
+);
