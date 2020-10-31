5ca241b470fc1c0ff3b31502fc39a56f3bff0e34
mouzam <mzmjuly9@gmail.com>
2020-10-31 19:48:22 +0530

    jsx set uped

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-ReactDOM.render(
-  <h1>Hello World</h1>,
-  document.getElementById('root')
-);
+ ReactDOM.render(
+   <div>
+     <h1>My foods</h1>
+     <ul>
+       <li>porotta</li>
+       <li>beef</li>
+       <li>chicken</li>
+     </ul>
+   </div>,
+   document.getElementById('root')
+ );
