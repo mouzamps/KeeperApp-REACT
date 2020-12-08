d69c2f2a4935b58d24cc192ce01fe98819fc4f77
mouzam <mzmjuly9@gmail.com>
2020-12-08 23:18:21 +0530

    inline style in jsx

--- src/index.js
+++ src/index.js
@@ -2,15 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const username = "Mouzam";
-const year = new Date().getFullYear()
+const hour = new Date().getHours()
+ let greeting;
+
+ const customStyle = {
+   color: ""
+ }
+ 
+
+ if(hour<12){
+   greeting = "GoodMorning"
+   customStyle.color = "red"
+
+ }else if (hour<14){
+   greeting = "GoodAfternoon"
+ }else{
+   greeting = "GoodNight"
+   customStyle.color= "red"
+ }
 
 ReactDOM.render(
   <div>
   <p className="para">Created by {username}</p>
-  <p>Copyright {year}</p>
-  <div>
-    <img src= ></img>
-  </div>
+  <p>Copyright {hour}</p>
+  <h1 style={customStyle}>{greeting}</h1>
+  
   </div>,
   document.getElementById('root')
 );
