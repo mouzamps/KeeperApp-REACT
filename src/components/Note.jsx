6a44641b0e572f4f96da483d8ec2d56ea55c6b06
mouzam <mzmjuly9@gmail.com>
2020-12-30 20:10:33 +0530

    part 2 done

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,9 +1,9 @@
 import React from "react"
 
-function Note (){
+function Note (props){
 return <div className="note">
-<h1>TITLE</h1>
-<p>CONTENTS ARE HERE</p>
+<h1>{props.title}</h1>
+<p>{props.content}</p>
 </div>
 
 }
